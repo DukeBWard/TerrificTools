e1505a193e03bfed5a3332b2db9afef7e7df17c4
bb6363 <78943026+bb6363@users.noreply.github.com>
2022-10-13 08:41:47 -0400

    Update Driver.py

--- src/Driver.py
+++ src/Driver.py
@@ -1,1 +1,13 @@
+# python, trying to remember
+
+# Login page before this???
+
+def driver():
+    
+    command = "a"
+    
+    while command != "Quit":
+        command = input("What's your command?: ")
+
+        # calling the appropriate command based on input
 
