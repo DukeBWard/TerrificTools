46dd37bd29ead5946ff6f01f18a954e07415027e
Luke Ward <lbw4459@rit.edu>
2022-10-23 17:55:06 -0400

    fixing the login stuff

--- src/controller/user_controller.py
+++ src/controller/user_controller.py
@@ -3,10 +3,11 @@ from models import user
 def login(cursor, user, password):
     cursor.execute(f"select * from user_table where username='{user}' and pass='{password}'")
     records = cursor.fetchone()
+    print(records)
     if records["username"] == user and records["pass"] == password:
-        user = user(records["userid"], records["createdate"], records["lastaccess"], records["username"], records["pass"], records["email"], records["reqid"])
-        print(user)
-        return user
+        current_user = user(records["userid"], None,None, records["username"], records["pass"], records["email"], records["reqid"])
+        print(current_user)
+        return True
     else:
         return False
     
