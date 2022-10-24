1666d6408dce4f012bdbc09da20ddf0ffe73be68
Luke Ward <lbw4459@rit.edu>
2022-10-24 13:30:15 -0400

    added signup stuff

--- src/Driver.py
+++ src/Driver.py
@@ -2,7 +2,6 @@
 
 # Login page before this???
 
-from TerrificTools.src.controller.user_controller import signup
 from controller import user_controller
 from models.user_model import user_model
 
@@ -10,24 +9,32 @@ def driver(conn, cursor):
     
     command = "a"
     
-    while command != "Quit":
+    while command.lower() != "quit":
         command = input("What's your command?: ")
         
         if command == "login":
             user = input("Username: ")
             password = input("Password: ")
             account = user_controller.login(conn, cursor, user, password)
-            if account == False: 
+            if not account: 
                 print("Login failed.")
                 signupIn = input("Would you like to sign up? (y/n)")
                 if signupIn.lower() == 'y':
                     user = input("Username: ")
                     password = input("Password: ")
                     email = input("Email: ")
-                    signup(conn, cursor, user, password, email)
-                elif signupIn.lower() == 'n':
+                    account = signup(conn, cursor, user, password, email)
+                    print("User: "+user+" signed up")
+                else:
                     return
-            
+        elif command.lower() == "signup" or command.lower() == "sign up":
+            user = input("Username: ")
+            password = input("Password: ")
+            email = input("Email: ")
+            account = signup(conn, cursor, user, password, email)
+            # 
+            # Continue from this point assuming logged in or signed up
+            # 
             command = input(account.username + ": ")
             
 
--- src/controller/user_controller.py
+++ src/controller/user_controller.py
@@ -1,3 +1,4 @@
+from re import L
 from models.user_model import user_model
 
 def login(conn, cursor, user, password):
@@ -13,9 +14,29 @@ def login(conn, cursor, user, password):
         return False
 
 def signup(conn, cursor, user, password, email):
-    cursor.execute(f"insert into user_table(createdate, lastaccess, username, pass, email) values\
-        (CURRENT_DATE, CURRENT_DATE, '{user}', '{password}', '{email}') ")
-    conn.commit()
-    return True
+    cursor.execute(f"select * from user_table where username='{user}' and pass='{password}'")
+    records = cursor.fetchone()
+    # Signup and login if not already signed up
+    if records["user"] == None and records["pass"] == None:
+        cursor.execute(f"insert into user_table(createdate, lastaccess, username, pass, email) values\
+            (CURRENT_DATE, CURRENT_DATE, '{user}', '{password}', '{email}') ")
+        conn.commit()
+        cursor.execute(f"select * from user_table where username='{user}' and pass='{password}'")
+        records = cursor.fetchone()
+        if records["username"] == user and records["pass"] == password:
+            userid = records["userid"]
+            current_user = user_model(records["userid"], records["createdate"],records["lastaccess"], records["username"], records["pass"], records["email"], records["reqid"])
+            cursor.execute(f"update user_table set lastaccess = CURRENT_DATE where userid ={userid}")
+            conn.commit()   # NEED THIS TO UPDATE/INSERT CURRENT CHANGES
+            return current_user
+    # Log in if already signed up
+    else:
+        if records["username"] == user and records["pass"] == password:
+            userid = records["userid"]
+            current_user = user_model(records["userid"], records["createdate"],records["lastaccess"], records["username"], records["pass"], records["email"], records["reqid"])
+            cursor.execute(f"update user_table set lastaccess = CURRENT_DATE where userid ={userid}")
+            conn.commit()   # NEED THIS TO UPDATE/INSERT CURRENT CHANGES
+            return current_user
+    
 
     
