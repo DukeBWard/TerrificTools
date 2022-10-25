e7f995a9dcf4a5486c21dab3b4d2f70ca2e28941
Atg6066 <98552535+AlexGraber22@users.noreply.github.com>
2022-10-25 09:26:46 -0400

    Create ticket works

--- src/controller/request_ticket_controller.py
+++ src/controller/request_ticket_controller.py
@@ -4,16 +4,12 @@ from models.request_ticket import request_ticket
 def create_ticket(conn,cursor,user,date_needed,duration,barcode):
     cursor.execute(f"select * from tools_table where barcode= '{barcode}'")
     tool = cursor.fetchone()
+    ownerid = tool["userid"]
     userid = user.userid
     if (tool["borrowed"] == True): 
-        cursor.execute(f"insert into request_ticket_table(dateneeded, duration, status, barcode, userid) values\
-            ('{date_needed}', '{duration}', False, '{barcode}', '{userid}') ")
+        cursor.execute(f"insert into request_ticket_table(dateneeded, duration, status, barcode, userid, toolowner) values\
+            ('{date_needed}', '{duration}', False, '{barcode}', '{userid}', '{ownerid}') ")
         conn.commit()
-        cursor.execute(f"select * from request_ticket_table where barcode = '{barcode}'")
-        records = cursor.fetchone()
-        reqid = records["reqid"]
-        cursor.execute(f"update user_table set reqid = {reqid} where userid = {userid}") # Might delete reqid from user table
-        conn.commit
     else:
         return False
     
